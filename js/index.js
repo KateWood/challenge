90712f9529aea5c65ba8645ffc320c5002dffbae
Kate Wood <katewood611@gmail.com>
2019-01-08 23:43:07 -0800

    adds photoFeed section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 $(function() {
-    
+  for (var i=0; i < 12; i++) {
+    var width = (Math.random() * 3 + 1) * 100;
+    $('.photoFeedImages').append('<img class="photoFeedImage" src="https://picsum.photos/' + width + '/200/?random"/>');
+    console.log('200 x ' + width);
+  }
 });
