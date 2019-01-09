1cce56f02f7472c0a1a0cbcb6e5f34a7bd2d809f
Kate Wood <katewood611@gmail.com>
2019-01-09 00:18:32 -0800

    spins testimonial images when play icon is clicked

--- js/index.js
+++ js/index.js
@@ -4,4 +4,11 @@ $(function() {
     $('.photoFeedImages').append('<img class="photoFeedImage" src="https://picsum.photos/' + width + '/200/?random"/>');
     console.log('200 x ' + width);
   }
+
+  $('.playIcon').on('click', function() {
+    $('.testimonial .image').toggleClass('rotate');
+    setTimeout(function(){
+      $('.testimonial .image').toggleClass('rotate');
+    }, 2000);
+  })
 });
