35f29cda012b2b63b392cfa8921130896d282365
Kate Wood <katewood611@gmail.com>
2019-01-09 01:54:32 -0800

    adds functionality to image slider

--- js/index.js
+++ js/index.js
@@ -10,5 +10,45 @@ $(function() {
     setTimeout(function(){
       $('.testimonial .image').toggleClass('rotate');
     }, 2000);
-  })
+  });
+
+  var imagesForSlider = [
+    ['https://picsum.photos/300/200/?random', 'https://picsum.photos/200/150/?random', 'https://picsum.photos/200/150/?random'],
+    ['./assets/halloween.jpg', './assets/cloud-racers.jpg', './assets/anniversary.jpg'],
+    ['https://picsum.photos/300/200/?random', 'https://picsum.photos/200/150/?random', 'https://picsum.photos/200/150/?random'],
+    ['https://picsum.photos/300/200/?random', 'https://picsum.photos/200/150/?random', 'https://picsum.photos/200/150/?random']
+  ];
+
+  var sliderTitle = ['THE PEOPLE', 'THE CULTURE', 'THE VALUES', 'THE WORK'];
+
+  var sliderSubtitle = ['Coworkers with a Variety of Talents', 'Perks of a Lifetime', 'A Company with Integrity', 'Creative Challenges'];
+
+  var dotNumber = 2;
+  setInterval(function() {
+    $('#dot' + dotNumber).click();
+    if (dotNumber < 3) {
+      dotNumber++;
+    } else {
+      dotNumber = 0;
+    }
+  }, 6000);
+
+  $('.dot').on('click', function() {
+    dotNumber = $(this).attr('id')[3];
+
+    $('.sliderTitle').text(sliderTitle[dotNumber]);
+    $('.sliderSubtitle').text(sliderSubtitle[dotNumber]);
+
+    var images = imagesForSlider[dotNumber];
+    $('.mainSliderImage').attr('src', images[0]);
+    $('.secondarySliderImage').eq(0).attr('src', images[1]);
+    $('.secondarySliderImage').eq(1).attr('src', images[2]);
+
+    $('.selected').toggleClass('selected');
+    $('#dot' + dotNumber).toggleClass('selected');
+  });
 });
+
+
+
+
